0e9329009b30c11d123f2872c31c8e9501cea6bb
Stefan Müller <s.mueller@it.kls-glt.de>
2016-01-16 15:26:16 +0100

    Much much better version. Configure all parts with systemd-nspawn.

--- 02.build_development_files.sh
+++ 02.build_development_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script must be called into the systemd-nspawn development container
+# This script set up the systemd-nspawn development container
 #
 EXAMPLE="./`basename $0` -s"
 #
@@ -8,28 +8,16 @@ EXAMPLE="./`basename $0` -s"
 # script verion, imcrement on change
 SCRIPTVERSION=0.1.7
 
-set -e
 
 
 # include generic functions (echo_b(), and debug() and so on)
 source ./lib/generic_functions.sh
-
-
-# Check for sudo
-check_sudo(){
-  debug "Check presents of sudo, install it if missing ..."
-  if [[ x`which sudo` = "x" ]]; then
-    debug "Error: sudo is not present. Trying to install it, hopefully we are root here"
-    run "apt-get update && apt-get install -y sudo"
-  fi
-}
+# sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development
+# sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/ && /bin/bash ./configure --prefix=/usr\"
 
 install_dependencies(){
   debug "Install dependencies for tools and kernel ..."
-  run "sudo apt-get install -y build-essential u-boot-tools"
-  run "sudo apt-get install -y libusb-1.0-0-dev git wget fakeroot kernel-package zlib1g-dev libncurses5-dev"
-  run "sudo apt-get install -y pkg-config"
-  run "make -j$[`nproc` + 1]"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development apt-get install -y build-essential pkg-config u-boot-tools libusb-1.0-0-dev git wget fakeroot kernel-package zlib1g-dev libncurses5-dev"
 }
 
 # # OPTION1: all files in so named Board Support Package (BSP), NOT USED!
@@ -47,97 +35,66 @@ install_dependencies(){
 build_uboot(){
   debug "Build U-Boot, boot loader ..."
   run "# https://github.com/LeMaker/u-boot-sunxi"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d u-boot-sunxi ]] && git clone https://github.com/LeMaker/u-boot-sunxi.git"
-  run "cd u-boot-sunxi"
-  run "git pull"
-  run "make BananaPro_config"
-  run "make -j$[`nproc` + 1]"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d u-boot-sunxi ]] && git clone https://github.com/LeMaker/u-boot-sunxi.git --depth=1 || true\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/u-boot-sunxi && git pull\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/u-boot-sunxi && make BananaPro_config\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/u-boot-sunxi && make -j$(nproc)\""
 }
 
 build_sunxi_tools(){
   debug "Build sunxi tools a.k.a. sun4i ..."
   run "# https://github.com/LeMaker/sunxi-tools"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d sunxi-tools ]] && git clone https://github.com/LeMaker/sunxi-tools.git"
-  run "cd sunxi-tools"
-  run "git pull"
-  run "make -j$[`nproc` + 1]"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d sunxi-tools  ]] && git clone https://github.com/LeMaker/sunxi-tools.git || true\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/sunxi-tools && git pull\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/sunxi-tools && make -j$(nproc)\""
 }
 
 build_sunxi_boards(){
-  debug "Build sys_config files for different sunxi boards ..."
+  debug "Fetch sunxi boards repo ..."
   run "# https://github.com/LeMaker/sunxi-boards"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d sunxi-boards ]] && git clone https://github.com/LeMaker/sunxi-boards.git"
-
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d sunxi-boards ]] && git clone https://github.com/LeMaker/sunxi-boards.git || true\""
 }
 
 get_fex_configuration(){
   debug "Fetch fex_configuration files (fex and bin) ..."
   run "# https://github.com/LeMaker/fex_configuration"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d fex_configuration ]] && git clone https://github.com/LeMaker/fex_configuration.git"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d fex_configuration ]] && git clone https://github.com/LeMaker/fex_configuration.git || true\""
 }
 
 build_linux_kernel(){
   debug "Fetch and build the linux kernel ..."
   run "# https://github.com/LeMaker/linux-sunxi"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d linux-sunxi ]] && git clone https://github.com/LeMaker/linux-sunxi.git --depth=1"
-  run "cd linux-sunxi"
-  run "git pull"
-  run "make ARCH=arm sun7i_defconfig"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d linux-sunxi ]] && git clone https://github.com/LeMaker/linux-sunxi.git --depth=1 || true\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/linux-sunxi && git pull\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/linux-sunxi && make sun7i_defconfig\""
   run "# FIXME: Include custom config"
-  run "# make ARCH=arm menuconfig"
-  run "make -j$[`nproc` + 1] ARCH=arm uImage modules"
-  run "make ARCH=arm INSTALL_MOD_PATH=output modules_install"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/linux-sunxi && make -j$(nproc) uImage modules\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/linux-sunxi && make INSTALL_MOD_PATH=output modules_install\""
 }
 
 build_libmodbus(){
   debug "Fetch and build libmodbus ..."
   run "# https://github.com/stephane/libmodbus.git"
-  run "sudo apt-get install -y autoconf git-core build-essential libtool"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d libmodbus ]] && git clone https://github.com/stephane/libmodbus.git --depth=1"
-  run "cd libmodbus"
-  run "git pull"
-  run "./autogen.sh"
-  run "./configure --prefix=/usr"
-  run "make -j$[`nproc` + 1]"
-  run "make install"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development apt-get install -y autoconf git-core build-essential libtool"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d libmodbus ]] && git clone https://github.com/stephane/libmodbus.git --depth=1 || true\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/libmodbus && git pull\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/libmodbus && ./autogen.sh\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/libmodbus && ./configure --prefix=/usr\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/libmodbus && make -j$(nproc)\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/libmodbus && make install\""
 }
 
 build_xmz(){
   debug "Fetch and build the xMZ-Mod-Touch GUI ..."
   run "# https://github.com/zzeroo/xMZ-Mod-Touch-GUI.git"
-  run "apt-get install -y libgtk-3-dev gsettings-desktop-schemas-dev libgee-dev libsqlite3-dev"
-  run "apt-get install -y libgirepository1.0-dev gnome-common valac"
-	run "cd ${OUTPUT_DIR}"
-  run "[[ ! -d xMZ-Mod-Touch-GUI ]] && git clone https://github.com/zzeroo/xMZ-Mod-Touch-GUI.git --depth=1"
-  run "cd xMZ-Mod-Touch-GUI"
-  run "git pull"
-  run "./autogen.sh --prefix=/usr"
-  run "make"
-  run "make dist"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development apt-get install -y intltool libgtk-3-dev gsettings-desktop-schemas-dev libgee-dev libsqlite3-dev libgirepository1.0-dev gnome-common valac"
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root && [[ ! -d xMZ-Mod-Touch-GUI ]] && git clone https://github.com/zzeroo/xMZ-Mod-Touch-GUI.git --depth=1 || true\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/xMZ-Mod-Touch-GUI && git pull\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/xMZ-Mod-Touch-GUI && ./autogen.sh --prefix=/usr\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/xMZ-Mod-Touch-GUI && make # -j$(nproc) dosn't work!\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/xMZ-Mod-Touch-GUI && make dist\""
+  run "sudo systemd-nspawn -D ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development /bin/bash -c \"cd /root/xMZ-Mod-Touch-GUI && make install\""
 }
-
-# Make a distribution tarball with the generated files, kernel and modules
-make_dist(){
-  debug "Create a tarball with the generated files, kernel and modules ..."
-	run "cd ${OUTPUT_DIR}"
-  run "mkdir files-kernel-modules/"
-  run "cp ./u-boot-sunxi/u-boot-sunxi-with-spl.bin files-kernel-modules/"
-  run "cp ./linux-sunxi/arch/arm/boot/uImage files-kernel-modules/"
-  run "cp ./fex_configuration/bin/banana_pro_7lcd.bin files-kernel-modules/"
-  run "cp -r ./linux-sunxi/output/lib/modules/3.4* files-kernel-modules/"
-  run "cp ./xMZ-Mod-Touch-GUI/xmz-*.tar.xz ./"
-  run "tar cfvz files-kernel-modules-${SCRIPTVERSION}.tgz files-kernel-modules/"
-  run "rm -rf files-kernel-modules"
-  run "# sudo cp /var/lib/container/${DISTRIBUTION}_armhf-development/root/files-kernel-modules-${SCRIPTVERSION}.tgz ."
-}
-
-
 
 
 # Main part of the script
@@ -145,10 +102,6 @@ make_dist(){
 # include option parser
 source ./lib/option_parser.sh
 
-
-
-
-check_sudo
 
 install_dependencies
 
@@ -170,7 +123,6 @@ build_libmodbus
 
 build_xmz
 
-make_dist
 
 
 
