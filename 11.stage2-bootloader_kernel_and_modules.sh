483082a6c1ae733df28b8b70605d2f08787837ce
Stefan Müller <s.mueller@it.kls-glt.de>
2016-02-29 16:04:31 +0100

    Exclude script.bin und .fex ab sid
    Der devicetree übernimmt die Rolle der script.bin

--- 11.stage2-bootloader_kernel_and_modules.sh
+++ 11.stage2-bootloader_kernel_and_modules.sh
@@ -31,23 +31,33 @@ create_loop_device_with_offset(){
 write_bootloader(){
   debug "Write bootloader ..."
 	run "sudo dd if=/dev/zero of=/dev/loop10 bs=1k count=1023 seek=1"
-	run "sudo dd if=${CONTAINER_DIR}/jessie_armhf-development/root/u-boot-sunxi/u-boot-sunxi-with-spl.bin of=/dev/loop10 bs=1024 seek=8"
+  if [ z${DISTRIBUTION} = "zsid" ]; then
+    run "sudo dd if=${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/u-boot/u-boot-sunxi-with-spl.bin of=/dev/loop10 bs=1024 seek=8"
+  else
+    run "sudo dd if=${CONTAINER_DIR}/jessie_armhf-development/root/u-boot-sunxi/u-boot-sunxi-with-spl.bin of=/dev/loop10 bs=1024 seek=8"
+  fi
 }
 
 create_boot_script(){
-  debug "Create boot script uEnv.txt ..."
-  export mnt=/tmp/disk
-  run "export mnt=/tmp/disk"
+  debug "Create boot script boot.cmd or uEnv.txt ..."
+
+  export mnt=/mnt/disk
+  run "export mnt=/mnt/disk"
+  # Check if dir is present, if not create
   run "[[ ! -d ${mnt}  ]] && sudo mkdir ${mnt}"
+  # Check if dir is already mounted, fail if so
+  run "mountpoint ${mnt} >/dev/null && error \"${mnt} ist schon gemounted\""
   run "sudo mount /dev/loop11 ${mnt}"
 
   if [ z${DISTRIBUTION} = "zsid" ]; then
-    run "cat <<-'EOF' |sudo tee ${mnt}/uEnv.txt
-bootargs=console=ttyS0,115200 disp.screen0_output_mode=EDID:1024x768p50 hdmi.audio=EDID:0 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait consoleblank=0
-aload_script=fatload mmc 0 0x43000000 script.bin;
-aload_kernel=fatload mmc 0 0x48000000 zImage;bootz 0x48000000;
-uenvcmd=run aload_script aload_kernel
+    run "cat <<-'EOF' |sudo tee ${mnt}/boot.cmd
+    # mkimage -C none -A arm -T script -d boot.cmd boot.scr
+    setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10
+    load mmc 0:1 0x43000000 \${fdtfile} || load mmc 0:1 0x43000000 boot/\${fdtfile}
+    load mmc 0:1 0x42000000 zImage || load mmc 0:1 0x42000000 boot/zImage
+    bootz 0x42000000 - 0x43000000
 EOF"
+    run "sudo mkimage -C none -A arm -T script -d ${mnt}/boot.cmd ${mnt}/boot.scr"
   else
     run "cat <<-'EOF' |sudo tee ${mnt}/uEnv.txt
 bootargs=console=ttyS0,115200 disp.screen0_output_mode=EDID:1024x768p50 hdmi.audio=EDID:0 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait consoleblank=0
@@ -58,21 +68,29 @@ EOF"
   fi
 }
 
+uboot_splash(){
+  if [ z${DISTRIBUTION} = "zsid" ]; then
+    debug "Copy in the splash image ..."
+    run "sudo cp share/u-boot-splashscreen.bmp ${mnt}"
+  fi
+}
+
 copy_in_kernel(){
   debug "Copy in kernel (partition1) ..."
   if [ z${DISTRIBUTION} = "zsid" ]; then
-    run "# sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/linux/arch/arm/boot/zImage ${mnt}/"
-    run "# sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/linux/arch/arm/boot/uImage ${mnt}/"
-    run "sudo cp ${CONTAINER_DIR}/jessie_armhf-development/root/linux-sunxi/arch/arm/boot/uImage ${mnt}/"
-    run "# sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/linux/arch/arm/boot/dts/sun7i-a20-bananapro.dts ${mnt}/"
+    run "sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/linux-sunxi/arch/arm/boot/zImage ${mnt}/"
+    run "sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/linux-sunxi/arch/arm/boot/dts/sun7i-a20-bananapro.dtb ${mnt}/"
   else
     run "# sudo cp ${CONTAINER_DIR}/jessie_armhf-development/root/linux-sunxi/arch/arm/boot/uImage ${mnt}/"
   fi
 }
 
 copy_in_script_bin(){
-  debug "Copy in script.bin ..."
-  run "sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/fex_configuration/bin/banana_pro_7lcd.bin ${mnt}/script.bin"
+  # Only need on jessie.
+  if [ z${DISTRIBUTION} = "zjessie" ]; then
+    debug "Copy in script.bin ..."
+    run "sudo cp ${CONTAINER_DIR}/${DISTRIBUTION}_armhf-development/root/fex_configuration/bin/banana_pro_7lcd.bin ${mnt}/script.bin"
+  fi
 }
 
 cleanup_mount(){
@@ -107,6 +125,8 @@ create_loop_device_with_offset
 write_bootloader
 
 create_boot_script
+
+uboot_splash
 
 copy_in_kernel
 
