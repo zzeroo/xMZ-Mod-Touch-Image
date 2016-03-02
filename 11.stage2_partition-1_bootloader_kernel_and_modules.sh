b4fb9e490d95d60064ced7edfe6db47c9001553e
Stefan Müller <s.mueller@it.kls-glt.de>
2016-03-02 12:50:12 +0100

    'quiet splash' Kernel Parameter hinzugefügt

--- 11.stage2_partition-1_bootloader_kernel_and_modules.sh
+++ 11.stage2_partition-1_bootloader_kernel_and_modules.sh
@@ -52,7 +52,7 @@ create_boot_script(){
   if [ z${DISTRIBUTION} = "zsid" ]; then
     run "cat <<-'EOF' |sudo tee ${mnt}/boot.cmd
     # mkimage -C none -A arm -T script -d boot.cmd boot.scr
-    setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 vt.global_cursor_default=0
+    setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 vt.global_cursor_default=0 quiet splash
     load mmc 0:1 0x41000000 u-boot-splashscreen.bmp
     setenv splashimage 0x41000000
     load mmc 0:1 0x43000000 \${fdtfile} || load mmc 0:1 0x43000000 boot/\${fdtfile}
