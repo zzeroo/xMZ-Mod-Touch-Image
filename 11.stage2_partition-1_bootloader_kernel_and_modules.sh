781c7a1a4650eb382f67fe76eaffc6839ee9d7ef
Stefan Müller <s.mueller@it.kls-glt.de>
2016-03-11 13:24:57 +0100

    u-boot splash details, leider noch nicht funktional

--- 11.stage2_partition-1_bootloader_kernel_and_modules.sh
+++ 11.stage2_partition-1_bootloader_kernel_and_modules.sh
@@ -52,9 +52,10 @@ create_boot_script(){
   if [ z${DISTRIBUTION} = "zsid" ]; then
     run "cat <<-'EOF' |sudo tee ${mnt}/boot.cmd
     # mkimage -C none -A arm -T script -d boot.cmd boot.scr
+    bootdelay=-2
     setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 vt.global_cursor_default=0 quiet splash
     load mmc 0:1 0x41000000 u-boot-splashscreen.bmp
-    setenv splashimage 0x41000000
+    setenv splashimage 41000000
     load mmc 0:1 0x43000000 \${fdtfile} || load mmc 0:1 0x43000000 boot/\${fdtfile}
     load mmc 0:1 0x42000000 zImage || load mmc 0:1 0x42000000 boot/zImage
     bootz 0x42000000 - 0x43000000
